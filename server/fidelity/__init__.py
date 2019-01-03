d79699b322109f3a518cd24279a5e2a003e7b192
olegpshenichniy <olegpshenichniy@gmail.com>
2019-01-03 16:29:57 +0200

    SDFID-517 Access privilege for the Compliance review view.

--- server/fidelity/__init__.py
+++ server/fidelity/__init__.py
@@ -1,5 +1,10 @@
+from superdesk import privilege
+
 
 def init_app(app):
     for resource in ['archive', 'published', 'archive_autosave']:
         app.config['DOMAIN'][resource]['schema']['extra']['mapping']['properties'] = (
             {'compliantlifetime': {'type': 'date'}})
+
+    privilege(name='fi_compliance_review', label='Fidelity compliance review',
+              description='User can access compliance review.')
