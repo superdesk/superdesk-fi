53ce141ae687d498e0a93b4614825d7afa0046fb
Tomas Kikutis <t.kikutis@gmail.com>
2019-01-28 14:09:40 +0100

    Implement review process for subject matter experts (sdfid 513) (#213)

--- client/fidelity/index.js
+++ client/fidelity/index.js
@@ -1,4 +1,7 @@
 import complianceReview from './compliance-review/controllers/ComplianceReviewController';
-import './compliance-review/styles/compliance-review.scss';
+import subjectMatterExpertReview from './subject-matter-expert-review/controllers/subjectMatterExpertReviewController';
 
-export default angular.module('fidelity.superdesk', [complianceReview.name]);
+import './compliance-review/styles/compliance-review.scss';
+import './subject-matter-expert-review/styles/subject-matter-expert-review.scss';
+
+export default angular.module('fidelity.superdesk', [complianceReview.name, subjectMatterExpertReview.name]);
