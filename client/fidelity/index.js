f4aeaeb6c856537e54e12383e835e38c32467b38
Tomas Kikutis <t.kikutis@gmail.com>
2019-11-05 11:37:33 +0100

    use new method for setting up extensions

--- client/fidelity/index.js
+++ client/fidelity/index.js
@@ -1,8 +1,17 @@
 import complianceReview from './compliance-review/controllers/ComplianceReviewController';
 import subjectMatterExpertReview from './subject-matter-expert-review/controllers/subjectMatterExpertReviewController';
+import {startApp} from 'superdesk-core/scripts/index';
+import annotationsLibraryExtension from 'superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension';
 
 import './compliance-review/styles/compliance-review.scss';
 import './subject-matter-expert-review/styles/subject-matter-expert-review.scss';
+
+setTimeout(() => {
+    startApp(
+        [annotationsLibraryExtension],
+        {},
+    );
+});
 
 export default angular
     .module('fidelity.superdesk', [complianceReview.name, subjectMatterExpertReview.name])
