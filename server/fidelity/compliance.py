12e533ed3f2d758be5411cef7faec82e83d03723
Holman Romero <holman.romero@gmail.com>
2019-11-27 15:26:41 +0100

    Replaces default compliance sentence to the new one provided by Fidelity

--- server/fidelity/compliance.py
+++ server/fidelity/compliance.py
@@ -17,7 +17,7 @@ import superdesk
 from superdesk.celery_app import celery
 
 logger = logging.getLogger(__name__)
-DEFAULT_EOL_TEXT = "This content is no longer updated by Fidelity International"
+DEFAULT_EOL_TEXT = "This content was correct at the time of publication and is no longer being updated."
 
 
 class ComplianceEOLCheck(superdesk.Command):
