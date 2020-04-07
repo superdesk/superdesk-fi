839f30b38972b8b9b6be0d5bdaa7ef17cad58268
olegpshenichniy <olegpshenichniy@gmail.com>
2020-04-07 17:52:01 +0200

    SDFID-601 Auto publish German content macro

--- server/fidelity/tests/set_german_and_publish_test.py
+++ server/fidelity/tests/set_german_and_publish_test.py
@@ -16,14 +16,16 @@ from fidelity.macros.set_german_and_publish import set_german_and_publish
 
 class SetGermanAndPublishTest(TestCase):
 
-    def test_set_german_and_publish(self):
+    def test_duplicate_and_publish(self):
         with self.app.app_context():
             with assert_raises(StopDuplication):
                 item = {
                     "_id": "test_123",
                     "guid": "test_123",
                     "type": "text",
+                    "headline": "zztop",
                     "language": "en",
+                    ITEM_STATE: CONTENT_STATE.PUBLISHED,
                     PUBLISH_SCHEDULE: {},
                     SCHEDULE_SETTINGS: {},
                 }
@@ -32,3 +34,60 @@ class SetGermanAndPublishTest(TestCase):
             new_item = archive_service.find_one(req=None, original_id='test_123')
             self.assertEqual(new_item['language'], 'de')
             self.assertEqual(new_item[ITEM_STATE], CONTENT_STATE.PUBLISHED)
+
+    def test_correction(self):
+        with self.app.app_context():
+            # DRY
+            self.test_duplicate_and_publish()
+
+            with assert_raises(StopDuplication):
+                item = {
+                    "_id": "test_123",
+                    "guid": "test_123",
+                    "type": "text",
+                    "headline": "zztop: first album",
+                    "language": "en",
+                    ITEM_STATE: CONTENT_STATE.CORRECTED,
+                    PUBLISH_SCHEDULE: {},
+                    SCHEDULE_SETTINGS: {},
+                }
+                set_german_and_publish(item)
+            archive_service = get_resource_service('archive')
+            new_item = archive_service.find_one(req=None, original_id='test_123')
+            self.assertEqual(new_item['language'], 'de')
+            self.assertEqual(new_item['headline'], 'zztop: first album')
+            self.assertEqual(new_item[ITEM_STATE], CONTENT_STATE.CORRECTED)
+
+    def test_language_constraint(self):
+        with self.app.app_context():
+            with assert_raises(StopDuplication):
+                item = {
+                    "_id": "test_123",
+                    "guid": "test_123",
+                    "type": "text",
+                    "language": "de",
+                    ITEM_STATE: CONTENT_STATE.PUBLISHED,
+                    PUBLISH_SCHEDULE: {},
+                    SCHEDULE_SETTINGS: {},
+                }
+                set_german_and_publish(item)
+            archive_service = get_resource_service('archive')
+            new_item = archive_service.find_one(req=None, original_id='test_123')
+            self.assertIsNone(new_item)
+
+    def test_state_constraint(self):
+        with self.app.app_context():
+            with assert_raises(StopDuplication):
+                item = {
+                    "_id": "test_123",
+                    "guid": "test_123",
+                    "type": "text",
+                    "language": "de",
+                    ITEM_STATE: CONTENT_STATE.PROGRESS,
+                    PUBLISH_SCHEDULE: {},
+                    SCHEDULE_SETTINGS: {},
+                }
+                set_german_and_publish(item)
+            archive_service = get_resource_service('archive')
+            new_item = archive_service.find_one(req=None, original_id='test_123')
+            self.assertIsNone(new_item)
