52d241b9d9fb563716ca6569ddb5604402babe44
Mark Pittaway <mark.pittaway@gmail.com>
2021-06-03 19:42:14 +1000

    [SDFID-617] Enable SAMS

--- client/fidelity/index.js
+++ client/fidelity/index.js
@@ -2,14 +2,16 @@ import complianceReview from './compliance-review/controllers/ComplianceReviewCo
 import subjectMatterExpertReview from './subject-matter-expert-review/controllers/subjectMatterExpertReviewController';
 import {startApp} from 'superdesk-core/scripts/index';
 import annotationsLibraryExtension from 'superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension';
+import samsExtension from 'superdesk-core/scripts/extensions/sams/dist/src/extension';
+import {SamsAttachmentsWidget} from 'superdesk-core/scripts/extensions/sams/dist/src/apps/samsAttachmentsWidget';
 
 import './compliance-review/styles/compliance-review.scss';
 import './subject-matter-expert-review/styles/subject-matter-expert-review.scss';
 
 setTimeout(() => {
     startApp(
-        [annotationsLibraryExtension],
-        {},
+        [annotationsLibraryExtension, samsExtension],
+        {AuthoringAttachmentsWidget: SamsAttachmentsWidget},
     );
 });
 
