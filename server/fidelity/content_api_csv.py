76620c50c60e7602121c7f791de3a06379411440
Petr Jasek <jasekpetr@gmail.com>
2021-08-27 09:50:14 +0200

    use generator

--- server/fidelity/content_api_csv.py
+++ server/fidelity/content_api_csv.py
@@ -6,7 +6,6 @@ from flask import request
 from eve.utils import ParsedRequest
 from settings import PUBLIC_TOKEN
 from .content_api_rss import get_authors, get_content_type
-from superdesk.timer import timer
 
 logger = logging.getLogger(__name__)
 blueprint = flask.Blueprint("csv", __name__)
@@ -16,8 +15,8 @@ SEPARATOR = ';'
 FILENAME = 'bi.csv'
 
 
-def get_csv(items):
-    output = [SEPARATOR.join([
+def get_csv(items, packages):
+    yield SEPARATOR.join([
         'Package',
         'Title',
         'Guid',
@@ -27,35 +26,31 @@ def get_csv(items):
         'Language',
         'Article Type',
         'Keyword',
-    ])]
-    packages = get_packages()
-    with timer("csv"):
-        for item in items:
-            name = item.get("headline") or item.get("name")
-            if not name:
-                continue
-            package = packages.get(item['_id'], {'name': ''}).get('name') or ''
-            firstpublished = item['firstpublished'].strftime("%d/%m/%Y")
-            versioncreated = item['versioncreated'].strftime("%d/%m/%Y")
-            language = item['language'].upper()
-            content_type = get_content_type(item)
-            authors = get_authors(item) or ['']
-            keywords = item.get("keywords") or ['']
-            for author in authors:
-                for keyword in keywords:
-                    output.append(SEPARATOR.join([
-                        package,
-                        name,
-                        item['_id'],
-                        firstpublished,
-                        versioncreated,
-                        author,
-                        language,
-                        content_type,
-                        keyword,
-                    ]))
-    logger.info("len %d %d", len(items), len(output) - 1)
-    return '\n'.join(output)
+    ]) + "\n"
+    for item in items:
+        name = item.get("headline") or item.get("name")
+        if not name:
+            continue
+        package = packages.get(item['_id'], {'name': ''}).get('name') or ''
+        firstpublished = item['firstpublished'].strftime("%d/%m/%Y")
+        versioncreated = item['versioncreated'].strftime("%d/%m/%Y")
+        language = item['language'].upper()
+        content_type = get_content_type(item)
+        authors = get_authors(item) or ['']
+        keywords = item.get("keywords") or ['']
+        for author in authors:
+            for keyword in keywords:
+                yield SEPARATOR.join([
+                    package,
+                    name,
+                    item['_id'],
+                    firstpublished,
+                    versioncreated,
+                    author,
+                    language,
+                    content_type,
+                    keyword,
+                ]) + "\n"
 
 
 def get_packages():
@@ -78,8 +73,8 @@ def index():
     req.args = {k: request.args.get(k) for k in request.args if k != "token"}
     req.max_results = MAX_ITEMS
     items = list(items_service.get(req, {}))
-    content = get_csv(items)
-    return flask.Response(content, mimetype="text/csv", headers={
+    packages = get_packages()
+    return flask.Response(get_csv(items, packages), mimetype="text/csv", headers={
         "Content-Disposition": f"attachment; filename={FILENAME}",
     })
 
