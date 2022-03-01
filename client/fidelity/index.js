5e17d8c14e796a00b8e9ef8d86d29769e86595a0
Tomas Kikutis <t.kikutis@gmail.com>
2022-03-01 12:10:57 +0100

    predefined text fields

--- client/fidelity/index.js
+++ client/fidelity/index.js
@@ -1,8 +1,6 @@
 import complianceReview from './compliance-review/controllers/ComplianceReviewController';
 import subjectMatterExpertReview from './subject-matter-expert-review/controllers/subjectMatterExpertReviewController';
 import {startApp} from 'superdesk-core/scripts/index';
-import annotationsLibraryExtension from 'superdesk-core/scripts/extensions/annotationsLibrary/dist/src/extension';
-import samsExtension from 'superdesk-core/scripts/extensions/sams/dist/src/extension';
 import {SamsAttachmentsWidget} from 'superdesk-core/scripts/extensions/sams/dist/src/apps/samsAttachmentsWidget';
 
 import './compliance-review/styles/compliance-review.scss';
@@ -10,8 +8,28 @@ import './subject-matter-expert-review/styles/subject-matter-expert-review.scss'
 
 setTimeout(() => {
     startApp(
-        [annotationsLibraryExtension, samsExtension],
-        {AuthoringAttachmentsWidget: SamsAttachmentsWidget},
+        [
+            {
+                id: 'predefinedTextField',
+                load: () => import('superdesk-core/scripts/extensions/predefinedTextField'),
+                configuration: {
+                    placeholderMapping: {
+                        'internal_id': 'extra.internal_id',
+                    },
+                },
+            },
+            {
+                id: 'annotationsLibrary',
+                load: () => import('superdesk-core/scripts/extensions/annotationsLibrary'),
+            },
+            {
+                id: 'samsExtension',
+                load: () => import('superdesk-core/scripts/extensions/sams'),
+            },
+        ],
+        {
+            AuthoringAttachmentsWidget: SamsAttachmentsWidget
+        },
     );
 });
 
